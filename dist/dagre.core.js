69b6a49f9755bd9737c3afb046b4b0dec6ded768
Andrejus Novatorovas <anovatorovas@alnasoft.lt>
2021-03-18 12:15:44 +0200

    remove automatic sorting of the same rank elements

--- dist/dagre.core.js
+++ dist/dagre.core.js
@@ -311,6 +311,7 @@ function debugOrdering(g) {
 }
 
 },{"./graphlib":7,"./lodash":10,"./util":29}],7:[function(require,module,exports){
+// eslint-disable-next-line no-redeclare
 /* global window */
 
 var graphlib;
@@ -844,6 +845,7 @@ function canonicalize(attrs) {
 }
 
 },{"./acyclic":2,"./add-border-segments":3,"./coordinate-system":4,"./graphlib":7,"./lodash":10,"./nesting-graph":11,"./normalize":12,"./order":17,"./parent-dummy-chains":22,"./position":24,"./rank":26,"./util":29}],10:[function(require,module,exports){
+// eslint-disable-next-line no-redeclare
 /* global window */
 
 var lodash;
@@ -1384,6 +1386,10 @@ function order(g) {
   var bestCC = Number.POSITIVE_INFINITY,
     best;
 
+  /* 
+  
+  COMMENTED OUT TO PRESERVE THE STABLE ORDERING OF ELEMENTS
+  
   for (var i = 0, lastBest = 0; lastBest < 4; ++i, ++lastBest) {
     sweepLayerGraphs(i % 2 ? downLayerGraphs : upLayerGraphs, i % 4 >= 2);
 
@@ -1395,6 +1401,8 @@ function order(g) {
       bestCC = cc;
     }
   }
+  
+  */
 
   assignOrder(g, best);
 }
@@ -2950,7 +2958,7 @@ function notime(name, fn) {
 }
 
 },{"./graphlib":7,"./lodash":10}],30:[function(require,module,exports){
-module.exports = "0.8.5";
+module.exports = "0.8.6-pre";
 
 },{}]},{},[1])(1)
 });
